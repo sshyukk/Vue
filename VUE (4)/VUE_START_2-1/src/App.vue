c86c901e55be06d14aabf3725aedbf6f178dbe97
sshyukk <sanghyuk0328@gmail.com>
2021-10-07 15:18:17 +0900

    feat(VUE(4)_START_2-1): eslint를 사용하여 코드 정리

--- VUE (4)/VUE_START_2-1/src/App.vue
+++ VUE (4)/VUE_START_2-1/src/App.vue
@@ -1,6 +1,12 @@
 <template>
-    <h1>{{ msg }}</h1>
-    <Hello />
+  <h1
+    v-if="msg" 
+    class="message" 
+    @click="log"
+  >
+    {{ msg }}
+  </h1>
+  <Hello />
 </template>
 <script>
 // 절대 경로를 사용

81c8687f94fb71148b3c35992d41a7f7b27f18d8
sshyukk <sanghyuk0328@gmail.com>
2021-10-07 15:39:34 +0900

    feat(VUE(4)_START_4): 파일명 변경 && 새로운 프로젝트 생성

---
+++ VUE (4)/VUE_START_4/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1
+    v-if="msg" 
+    class="message" 
+    @click="log">
+    {{ msg }}
+  </h1>
+  <Hello />
+</template>
+<script>
+// 절대 경로를 사용
+import Hello from '~/components/Hello'
+
+export default {
+    components: {
+        Hello
+    },
+    data() {
+        return {
+            msg: 'Hello Vue!'
+        }
+    },
+}
+</script>
+

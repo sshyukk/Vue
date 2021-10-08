7b8a1397896ed0abe71214aff9da5d1f859f5100
sshyukk <sanghyuk0328@gmail.com>
2021-10-08 16:09:47 +0900

    feat(VUE(4)_START_5-4): props와 emit의 부모와 자식 간의 관계

---
+++ VUE (4)/VUE_START_5-4/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>
+    {{ msg }}
+  </h1>
+  <Hello
+    :message="msg"
+    @please="reverseMsg" />
+</template>
+
+<script>
+import Hello from '~/components/Hello'
+
+export default {
+  components: {
+    Hello
+  },
+  data() {
+    return {
+      msg: 'Hello Vue!'
+      }
+    },
+    methods: {
+      reverseMsg() {
+        this.msg = this.msg
+          .split('')
+          .reverse()
+          .join('')
+      }
+    }
+  }
+</script>
+
---
+++ VUE (4)/VUE_START_5-4/src/components/Hello.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1 @click="$emit('please')">
+    {{ message }}
+  </h1>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+$color: red;
+h1 {
+    color: $color;
+}
+</style>

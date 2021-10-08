2ccb40e32743b7427030fed112517d159d336646
sshyukk <sanghyuk0328@gmail.com>
2021-10-08 15:43:29 +0900

    feat(VUE(4)_START_5-3): 컴포넌트 속성 상속하기

---
+++ VUE (4)/VUE_START_5-3/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>
+    {{ msg }}
+  </h1>
+  <Hello
+    class="hello"
+    style="font-size: 100px;"
+    @click="msg += '!'" />
+</template>
+
+<script>
+import Hello from '~/components/Hello'
+
+export default {
+  components: {
+    Hello
+  },
+  data() {
+    return {
+      msg: 'Hello Vue!'
+      }
+    }
+  }
+</script>
+
---
+++ VUE (4)/VUE_START_5-3/src/components/Hello.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1
+    :class="$attrs.class"
+    :style="$attrs.style"
+    @click="$attrs.onClick">
+    Hello
+  </h1>
+  <h2 @click="$attrs.onClick">
+    Haha?!
+  </h2>
+</template>
+
+<style scoped lang="scss">
+$color: red;
+h1 {
+    color: $color;
+}
+</style>

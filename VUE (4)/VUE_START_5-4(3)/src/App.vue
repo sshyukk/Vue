f242c04f2e7722da2ed05c44da6e97c328cdd712
sshyukk <sanghyuk0328@gmail.com>
2021-10-08 16:37:10 +0900

    feat(VUE(4)_START_5-4): emit 커스텀 이벤트로 양방향 이벤트 구현

---
+++ VUE (4)/VUE_START_5-4(3)/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>
+    {{ msg }}
+  </h1>
+  <Hello
+    :message="msg"
+    @update="msg = $event" />
+</template>
+
+<script>
+import Hello from '~/components/Hello'
+
+export default {
+  components: {
+    Hello
+  },
+  data() {
+    return {
+      msg: 'Hello Vue!'
+      }
+    }
+  }
+</script>
+
---
+++ VUE (4)/VUE_START_5-4(3)/src/components/Hello.vue
@@ -0,0 +1,26 @@
+<template>
+  <label>
+    <input
+      :value="message"
+      @input="$emit('update', $event.target.value)" />
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      default: ''
+    }
+  },
+  emits: ['update']
+  }
+</script>
+
+<style scoped lang="scss">
+$color: red;
+h1 {
+    color: $color;
+}
+</style>

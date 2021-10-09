5343d0cdb6861d725cc495b85dc26a07699fe69e
sshyukk <sanghyuk0328@gmail.com>
2021-10-09 19:55:08 +0900

    feat(VUE(5)_START_1): fetch 플러그인 사용 정리

---
+++ VUE(5)/VUE_START_1/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from '~/App'
+import fetchPlugin from '~/plugins/fetch'
+
+
+const app = createApp(App)
+app.use(fetchPlugin, {
+    
+})
+app.mount('#app')

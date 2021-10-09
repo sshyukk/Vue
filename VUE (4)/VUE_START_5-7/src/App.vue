25045d487433c9d904dd56076ae4893d2bc42126
sshyukk <sanghyuk0328@gmail.com>
2021-10-09 18:58:45 +0900

    feat(VUE(4)_START_5-7): ref 참조 속성 사용

---
+++ VUE (4)/VUE_START_5-7/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>
+    {{ msg }}
+  </h1>
+  <Hello />
+</template>
+
+<script>
+import Hello from '~/components/Hello'
+
+export default {
+  components: {
+    Hello
+  },
+  data() {
+    return {
+      msg: 'Hello Vue!',
+      }
+    }
+  }
+</script>
+
---
+++ VUE (4)/VUE_START_5-7/src/components/Hello.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 ref="hello">
+    Hello
+  </h1>
+</template>
+
+<script>
+export default {
+  // html 구조가 생성된 이후에 참조하는 것이기 때문에 당연히 created 라이프 사이클에서는 사용이 불가능하다.
+  mounted() {
+    const h1El = this.$refs.hello
+    console.log(h1El)
+  }
+}
+</script>
+
+<style scoped lang="scss">
+$color: red;
+h1 {
+    color: $color;
+}
+</style>

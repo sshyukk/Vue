d7f9db76dcaf6ec28876c088a52693ed9611709c
sshyukk <sanghyuk0328@gmail.com>
2021-10-10 18:34:01 +0900

    feat(VUE(5)_START_5): provide, inject 기능 사용 - 후손 객체에게 데이터 전달

---
+++ VUE (5)/VUE_START_5/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>
+    {{ msg }}
+  </h1>
+  <Parent />
+</template>
+
+<script>
+import Parent from '~/components/Parent'
+export default {
+  components: {
+    Parent
+  },
+  provide() {
+    return {
+      msg: this.msg
+    }
+  },
+  data() {
+    return {
+      msg: 'Hello Vue!'
+    }
+  }
+  
+}
+</script>
+
---
+++ VUE (5)/VUE_START_5/src/components/Child.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2>Child! / {{ msg }}</h2>
+</template>
+
+<script>
+export default {
+    inject: ['msg']
+}
+</script>
